0360fc631c6b189f7aeadd8bf73ccb07c83892c2
aman tandon <aman.tandon3096@gmail.com>
2020-01-30 00:33:51 +0530

    commit

---
+++ my-app/src/components/DishDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DishDetail extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Dish Detail Component</h1>
+            </div>
+        )
+    }
+}
